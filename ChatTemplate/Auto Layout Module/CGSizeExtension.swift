2dbeaf5cbacecdddafaba507ea64230b55a15808
Bibin Jacob <bibinjacob123@gmail.com>
2018-09-06 12:56:42 +0530

    Separated Auto Layout Module

---
+++ ChatTemplate/Auto Layout Module/CGSizeExtension.swift
@@ -0,0 +1,17 @@
+//
+//  CGSizeExtension.swift
+//  ChatTemplate
+//
+//  Created by Bibin Jacob Pulickal on 06/09/18.
+//  Copyright © 2018 Bibin Jacob Pulickal. All rights reserved.
+//
+
+import UIKit
+
+extension CGSize {
+    init(width: CGFloat = 0, height: CGFloat = 0) {
+        self.init()
+        self.width = width
+        self.height = height
+    }
+}
