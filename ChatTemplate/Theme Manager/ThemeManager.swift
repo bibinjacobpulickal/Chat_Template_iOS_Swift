8bfde546d2e1289f25488b03e9efb54b2a7f6cb2
Bibin Jacob <bibinjacob123@gmail.com>
2018-09-10 11:29:39 +0530

    Added setups to theme manager

--- ChatTemplate/Theme Manager/ThemeManager.swift
+++ ChatTemplate/Theme Manager/ThemeManager.swift
@@ -26,12 +26,12 @@ class ThemeManager {
         let controlBackground = UIImage(named: "controlBackground")?.withRenderingMode(.alwaysTemplate)
             .resizableImage(withCapInsets: UIEdgeInsets(top: 3, left: 3, bottom: 3, right: 3))
         
-        setSegmentedControl(controlBackground)
-        setStepper(controlBackground)
-        setNavigationBar(theme)
-        setTabBar(theme)
-        setSwitch(theme)
-        setSlider()
+        setupSegmentedControl(controlBackground)
+        setupStepper(controlBackground)
+        setupNavigationBar(theme)
+        setupTabBar(theme)
+        setupSwitch(theme)
+        setupSlider()
     }
     
     private static func setUserDefaults(_ theme: Theme) {
@@ -39,7 +39,7 @@ class ThemeManager {
         UserDefaults.standard.synchronize()
     }
     
-    private static func setNavigationBar(_ theme: Theme) {
+    private static func setupNavigationBar(_ theme: Theme) {
         UINavigationBar.appearance().barStyle = theme.barStyle
         UINavigationBar.appearance().tintColor = theme.titleTextColor
         UINavigationBar.appearance().setBackgroundImage(theme.navigationBackgroundImage, for: .default)
@@ -47,7 +47,7 @@ class ThemeManager {
         UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage(named: "backArrowMaskFixed")
     }
     
-    private static func setTabBar(_ theme: Theme) {
+    private static func setupTabBar(_ theme: Theme) {
         UITabBar.appearance().barStyle = theme.barStyle
         UITabBar.appearance().backgroundImage = theme.tabBarBackgroundImage
         
@@ -56,7 +56,7 @@ class ThemeManager {
         UITabBar.appearance().selectionIndicatorImage = tabResizableIndicator
     }
     
-    private static func setStepper(_ controlBackground: UIImage?) {
+    private static func setupStepper(_ controlBackground: UIImage?) {
         UIStepper.appearance().setBackgroundImage(controlBackground, for: .normal)
         UIStepper.appearance().setBackgroundImage(controlBackground, for: .disabled)
         UIStepper.appearance().setBackgroundImage(controlBackground, for: .highlighted)
@@ -64,7 +64,7 @@ class ThemeManager {
         UIStepper.appearance().setIncrementImage(UIImage(named: "morePaws"), for: .normal)
     }
     
-    private static func setSlider() {
+    private static func setupSlider() {
         UISlider.appearance().setThumbImage(UIImage(named: "sliderThumb"), for: .normal)
         UISlider.appearance().setMaximumTrackImage(UIImage(named: "maximumTrack")?
             .resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 0.0, bottom: 0, right: 6.0)), for: .normal)
@@ -73,12 +73,12 @@ class ThemeManager {
             .resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 6.0, bottom: 0, right: 0)), for: .normal)
     }
     
-    private static func setSwitch(_ theme: Theme) {
+    private static func setupSwitch(_ theme: Theme) {
         UISwitch.appearance().onTintColor = theme.mainColor.withAlphaComponent(0.3)
         UISwitch.appearance().thumbTintColor = theme.mainColor
     }
     
-    private static func setSegmentedControl(_ controlBackground: UIImage?) {
+    private static func setupSegmentedControl(_ controlBackground: UIImage?) {
         
         let controlSelectedBackground = UIImage(named: "controlSelectedBackground")?
             .withRenderingMode(.alwaysTemplate)
